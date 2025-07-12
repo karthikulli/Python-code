cd2946bd57dd912ae3f4ad0bace86b5cc016f37b
karthikulli <vullikarthik2@gmail.com>
2025-07-13 00:35:34 +0530

    Create phone_number.py

---
+++ phone_number.py
@@ -0,0 +1,14 @@
+First=int(input("Enter first three digits: \n"))
+one=First*40
+two=one*25
+Second=int(input("Enter next three digits: \n"))
+three=two+Second
+four=three*50
+five=four+1
+six=five*400
+Third=int(input("Enter last four digits: \n"))
+seven=six+Third
+eight=seven+Third
+nine=int(eight/2)
+ten=nine-200
+print(ten)
